10fe8714e90cd0201a3a33fbba459c8e8350e2be
Aliaksandr Bykouski <developweb34@gmail.com>
2025-06-25 22:51:52 +0200

    editquestionpage full

--- src/pages/EditQuestionPage/EditQuestion.jsx
+++ src/pages/EditQuestionPage/EditQuestion.jsx
@@ -6,6 +6,8 @@ import delayFn from "../../helpers/delayFn.js";
 import { API_URL } from "../../constants/index.js";
 import { toast } from "react-toastify";
 import dateFormat from "../../helpers/dateFormat.js";
+import useFetch from "../../hooks/useFetch.js";
+import { useNavigate } from "react-router-dom";
 
 const editCardQuestion = async (_prevState, formData) => {
   try {
@@ -42,19 +44,36 @@ const editCardQuestion = async (_prevState, formData) => {
 };
 
 const EditQuestion = ({ initialState = {} }) => {
+  const navigate = useNavigate();
   const [formState, formAction, isPending] = useActionState(editCardQuestion, {
     ...initialState,
     clearForm: false,
   });
+  const [removeQuestion, isQuestionRemoving] = useFetch(async () => {
+    await fetch(`${API_URL}/react/${initialState.id}`, {
+      method: "DELETE",
+    });
+
+    toast.success(" The Question has been  successfully removed");
+
+    navigate("/");
+  });
+
+  const onRemoveQuestionHandler = () => {
+    const isRemove = confirm("Are you sure you want to remove this question?");
+    if (isRemove) {
+      removeQuestion();
+    }
+  };
   return (
     <div className={classes["edit-question-page"]}>
-      {isPending && <Loader />}
+      {(isPending || isQuestionRemoving) && <Loader />}
       <h1 className={classes["edit-question-page--title"]}>Edit question</h1>
       <div className={classes["edit-question-page--form-wrapper"]}>
         <button
           className={classes["edit-question-page--close"]}
-          disabled={isPending}
-          onClick={() => {}}
+          disabled={isPending || isQuestionRemoving}
+          onClick={onRemoveQuestionHandler}
         >
           X
         </button>
@@ -62,7 +81,7 @@ const EditQuestion = ({ initialState = {} }) => {
         <QuestionForm
           formState={formState}
           formAction={formAction}
-          isPending={isPending}
+          isPending={isPending || isQuestionRemoving}
           submitBtnText="Edit question"
           initialState={initialState}
         />
