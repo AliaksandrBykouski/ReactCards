07e8bb9557d866f7c48b9b2e12919fa16c92ad0e
Aliaksandr Bykouski <developweb34@gmail.com>
2025-06-26 13:41:41 +0200

    add isAuth, AuthProvider, logic Auth

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,24 @@ import NotFoundPage from "./pages/NotFoundPage";
 import QuestionPage from "./pages/QuestionPage";
 import { AddQuestionPageLazy } from "./pages/AddQuestionPage";
 import EditQuestionPage from "./pages/EditQuestionPage";
+import { AuthProvider } from "./auth/AuthProvider";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="forbidden" element={<div>🍻I NEED MORE BEER🍻</div>} />
-          <Route path="addquestion" element={<AddQuestionPageLazy />} />
-          <Route path="question/:id" element={<QuestionPage />} />
-          <Route path="/editquestion/:id" element={<EditQuestionPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="forbidden" element={<div>🍻I NEED MORE BEER🍻</div>} />
+            <Route path="addquestion" element={<AddQuestionPageLazy />} />
+            <Route path="question/:id" element={<QuestionPage />} />
+            <Route path="/editquestion/:id" element={<EditQuestionPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,15 @@ import ReactLogo from "../../assets/react.svg";
 import classes from "./Header.module.scss";
 import Button from "../Button/index.jsx";
 import { useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth.js";
 
 const Header = () => {
   const navigate = useNavigate();
+  const { isAuth, setIsAuth } = useAuth();
+
+  const loginHandler = () => {
+    setIsAuth(!isAuth);
+  };
   return (
     <header className={classes.header}>
       <div className={classes["header-logo"]} onClick={() => navigate("/")}>
@@ -13,7 +19,7 @@ const Header = () => {
       </div>
       <div className={classes["header-btns"]}>
         <Button onClick={() => navigate("/addquestion")}>Add</Button>
-        <Button>Log in</Button>
+        <Button onClick={loginHandler}>{isAuth ? "Logout" : "Login"}</Button>
       </div>
     </header>
   );
